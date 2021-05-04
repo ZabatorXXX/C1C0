eca9e51e328e99e7cff40056fe7e568ff1633791
ZabatorXXX <Zabatorxv@gmail.com>
2021-05-04 14:02:43 +0200

    WIP

--- Calculator.py
+++ Calculator.py
@@ -1,12 +1,12 @@
-# class Calc:
-# def add(x, y):
-# return x + y
+class Calc:
+    def add(x, y):
+        return x + y
 
-# def subtract(x, y):
-# return x - y
+    def subtract(x, y):
+        return x - y
 
-# def multi(x, y):
-# return x * y
+    def multi(x, y):
+        return x * y
 
-# def div(x, y):
-# return x / y
+    def div(x, y):
+        return x / y
