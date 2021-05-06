6d44910d1c3b6e1d23fd509c7fd5ab24260aa5e9
ZabatorXXX <Zabatorxv@gmail.com>
2021-05-06 09:14:58 +0200

    W4p

--- C4lcul4torcopy.py
+++ C4lcul4torcopy.py
@@ -1,16 +1,21 @@
 result = []
 
+
 def addition(x, y):
     return x + y
+
 
 def subtract(x, y):
     return x - y
 
+
 def multiply(x, y):
     return x * y
 
+
 def divide(x, y):
     return x / y
+
 
 print("Choose numbers 1, 2, 3, 4")
 print("1.Add")
